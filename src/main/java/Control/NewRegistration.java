359acf2e668c80b46ebe2e2c0b03a93aea4b8e5f
根本　晴輝 <bp23014@shibaura-it.ac.jp>
2025-12-17 13:50:35 +0900

    NewRegistrationクラス作成

--- src/main/java/Control/NewRegistration.java
+++ src/main/java/Control/NewRegistration.java
@@ -1,4 +1,53 @@
 package Control;
 
+import Communication.DatabaseServerCommunication;
+
 public class NewRegistration {
+
+    private String userID;
+    private String password;
+
+    public NewRegistration(String userID, String password) {
+        this.userID = userID;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkUserID(){
+        if(userID == null) return false;
+        if(userID.isEmpty()) return false;
+        return true;
+    }
+
+    public boolean checkPassword() {
+        if (password == null) return false;
+        if (password.isEmpty()) return false;
+
+        return true;
+    }
+
+
+    /*
+    public void registerToDatabase() {
+        // 入力チェック
+        if (!checkUserID() || !checkPassword()) {
+            throw new IllegalStateException("UserID or Password is invalid");
+        }
+
+
+
+        // DBサーバとの通信
+        DatabaseServerCommunication dbComm = new DatabaseServerCommunication();
+        dbComm.processQuery(userID, password);
+    }
+*/
+
+
 }
